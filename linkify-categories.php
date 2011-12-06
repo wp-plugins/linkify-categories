0dd29ce53a237319e9dab9b7ea0437822628384e
coffee2code <coffee2code@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-12-06 05:04:34 +0000

    * Update to v2.0.2 * Note compatibility through WP 3.3+ * Add link to plugin directory page to readme.txt * Update copyright date (2012)
    git-svn-id: https://plugins.svn.wordpress.org/linkify-categories/tags/2.0.2@471425 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- linkify-categories.php
+++ linkify-categories.php
@@ -2,76 +2,30 @@
 /**
  * @package Linkify_Categories
  * @author Scott Reilly
- * @version 1.1
+ * @version 2.0.2
  */
 /*
 Plugin Name: Linkify Categories
-Version: 1.1
-Plugin URI: http://coffee2code.com/wp-plugins/linkify-categories
+Version: 2.0.2
+Plugin URI: http://coffee2code.com/wp-plugins/linkify-categories/
 Author: Scott Reilly
 Author URI: http://coffee2code.com
 Description: Turn a list of category IDs and/or slugs into a list of links to those categories.
 
-Compatible with WordPress 2.8+, 2.9+.
+Compatible with WordPress 2.8+, 2.9+, 3.0+, 3.1+, 3.2+, 3.3+.
 
-=>> Read the accompanying readme.txt file for more information.  Also, visit the plugin's homepage
-=>> for more information and the latest updates
-
-Installation:
-
-1. Download the file http://coffee2code.com/wp-plugins/linkify-categories.zip and unzip it into your 
-/wp-content/plugins/ directory (or install via the built-in WordPress plugin installer).
-2. Activate the plugin through the 'Plugins' admin menu in WordPress
-3. Use the linkify_categories() template tag in one of your templates (be sure to pass it at least the first argument
-	indicating what category IDs and/or slugs to linkify -- the argument can be an array, a space-separate list, or a
-	comma-separated list).  Other optional arguments are available to customize the output.
-
-
-Examples:
-
-These are all valid calls:
-	<?php linkify_categories(43); ?>
-	<?php linkify_categories("43"); ?>
-	<?php linkify_categories("books"); ?>
-	<?php linkify_categories("43 92 102"); ?>
-	<?php linkify_categories("book movies programming-notes"); ?>
-	<?php linkify_categories("book 92 programming-notes"); ?>
-	<?php linkify_categories("43,92,102"); ?>
-	<?php linkify_categories("book,movies,programming-notes"); ?>
-	<?php linkify_categories("book,92,programming-notes"); ?>
-	<?php linkify_categories("43, 92, 102"); ?>
-	<?php linkify_categories("book, movies, programming-notes"); ?>
-	<?php linkify_categories("book, 92, programming-notes"); ?>
-	<?php linkify_categories(array(43,92,102)); ?>
-	<?php linkify_categories(array("43","92","102")); ?>
-	<?php linkify_categories(array("book","movies","programming-notes")); ?>
-	<?php linkify_categories(array("book",92,"programming-notes")); ?>
-
-<?php linkify_categories("43 92"); ?>
-Displays something like:
-	<a href="http://yourblog.com/category/books">Books</a>, 
-	<a href="http://yourblog.com/category/movies">Movies</a>
-
-<ul><?php linkify_categories("43, 92", "<li>", "</li>", "</li><li>"); ?></ul>
-Displays something like:
-	<ul><li><a href="http://yourblog.com/category/books">Books</a></li>
-	<li><a href="http://yourblog.com/category/movies">Movies</a></li></ul>
-
-<?php linkify_categories(""); // Assume you passed an empty string as the first value ?>
-Displays nothing.
-
-<?php linkify_categories("", "", "", "", "", "No related categories."); // Assume you passed an empty string as the first value ?>
-Displays:
-	No related categories.
+=>> Read the accompanying readme.txt file for instructions and documentation.
+=>> Also, visit the plugin's homepage for additional information and updates.
+=>> Or visit: http://wordpress.org/extend/plugins/linkify-categories/
 
 */
 
 /*
-Copyright (c) 2009-2010 by Scott Reilly (aka coffee2code)
+Copyright (c) 2009-2012 by Scott Reilly (aka coffee2code)
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation 
-files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, 
-modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the 
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy,
+modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
 Software is furnished to do so, subject to the following conditions:
 
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
@@ -82,38 +36,53 @@ LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRA
 IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
+require_once( dirname( __FILE__ ) . '/linkify-categories.widget.php' );
+
+if ( ! function_exists( 'c2c_linkify_categories' ) ) :
 /**
  * Displays links to each of any number of categories specified via category IDs and/or slugs
- * 
+ *
+ * @since 2.0
+ *
  * @param int|string|array $categories A single category ID/slug, or multiple category IDs/slugs defined via an array, or multiple category IDs/slugs defined via a comma-separated and/or space-separated string
  * @param string $before (optional) Text to appear before the entire category listing (if categories exist or if 'none' setting is specified)
  * @param string $after (optional) Text to appear after the entire category listing (if categories exist or if 'none' setting is specified)
  * @param string $between (optional) Text to appear between all categories
- * @param string $before_last (optional) Text to appear between the second-to-last and last element, if not specified, 'between' value is used	
+ * @param string $before_last (optional) Text to appear between the second-to-last and last element, if not specified, 'between' value is used
  * @param string $none (optional) Text to appear when no categories have been found.  If blank, then the entire function doesn't display anything
  * @return none (Text is echoed; nothing is returned)
  */
-function linkify_categories( $categories, $before = '', $after = '', $between = ', ', $before_last = '', $none = '' ) {
-	if ( empty($categories) )
+function c2c_linkify_categories( $categories, $before = '', $after = '', $between = ', ', $before_last = '', $none = '' ) {
+	if ( empty( $categories ) )
 		$categories = array();
-	elseif ( !is_array($categories) )
-		$categories = explode(',', str_replace(array(', ', ' ', ','), ',', $categories));
+	elseif ( ! is_array( $categories ) )
+		$categories = explode( ',', str_replace( array( ', ', ' ', ',' ), ',', $categories ) );
 
-	if ( empty($categories) ) $response = '';
-	else {
+	if ( empty( $categories ) ) {
+		$response = '';
+	} else {
 		$links = array();
 		foreach ( $categories as $id ) {
-			if ( 0 == (int)$id ) {
-				$cat = get_category_by_slug($id);
-				$id = $cat->cat_ID;
+			if ( 0 == (int) $id ) {
+				$cat = get_category_by_slug( $id );
+				if ( $cat )
+					$id = $cat->cat_ID;
 			}
-			$title = get_cat_name($id);
+			if ( ! $id )
+				continue;
+			$title = get_cat_name( $id );
 			if ( $title )
-				$links[] = '<a href="' . get_category_link($id) . '" title="' . esc_attr(sprintf(__( "View all posts in %s"), $title)) . '">' . $title . '</a>';
+				$links[] = sprintf(
+					'<a href="%1$s" title="%2$s">%3$s</a>',
+					get_category_link( $id ),
+					esc_attr( sprintf( __( "View all posts in %s" ), $title ) ),
+					$title
+				);
 		}
-		if ( empty($before_last) ) $response = implode($links, $between);
-		else {
-			switch ( $size = sizeof($links) ) {
+		if ( empty( $before_last ) ) {
+			$response = implode( $between, $links );
+		} else {
+			switch ( $size = sizeof( $links ) ) {
 				case 1:
 					$response = $links[0];
 					break;
@@ -121,14 +90,32 @@ function linkify_categories( $categories, $before = '', $after = '', $between = 
 					$response = $links[0] . $before_last . $links[1];
 					break;
 				default:
-					$response = implode(array_slice($links,0,$size-1), $between) . $before_last . $links[$size-1];
+					$response = implode( $between, array_slice( $links, 0, $size-1 ) ) . $before_last . $links[$size-1];
 			}
 		}
 	}
-	if ( empty($response) ) {
-		if ( empty($none) ) return;
+	if ( empty( $response ) ) {
+		if ( empty( $none ) )
+			return;
 		$response = $none;
 	}
 	echo $before . $response . $after;
 }
+add_action( 'c2c_linkify_categories', 'c2c_linkify_categories', 10, 6 );
+endif;
+
+if ( ! function_exists( 'linkify_categories' ) ) :
+/**
+ * Displays links to each of any number of categories specified via category IDs and/or slugs
+ *
+ * @since 1.0
+ * @deprecated 2.0 Use c2c_linkify_categories() instead
+ */
+function linkify_categories( $categories, $before = '', $after = '', $between = ', ', $before_last = '', $none = '' ) {
+	_deprecated_function( 'linkify_categories', '2.0', 'c2c_linkify_categories' );
+	return c2c_linkify_categories( $categories, $before, $after, $between, $before_last, $none );
+}
+add_action( 'linkify_categories', 'linkify_categories', 10, 6 ); // Deprecated
+
+endif;
 ?>
